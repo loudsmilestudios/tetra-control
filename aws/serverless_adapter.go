e0e9ecd364625dd316dcabfedbc270ccc7707693
Manley <j@cloudsumu.com>
2021-11-07 04:12:00 +0000

    #2 Implement AWS Server Management framework

---
+++ aws/serverless_adapter.go
@@ -0,0 +1,43 @@
+package aws
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// ServerlessAdapter implements functions to allow TetraControl to run serverlessly
+type ServerlessAdapter struct{}
+
+// Start passes core.Router to AWS Lambda
+func (adapter *ServerlessAdapter) Start() {
+	lambda.StartHandler(adapter)
+}
+
+// Invoke works as the entrypoint to the lambda function and marshalls the event
+func (adapter *ServerlessAdapter) Invoke(ctx context.Context, event []byte) ([]byte, error) {
+
+	request := &events.APIGatewayProxyRequest{}
+	if err := json.Unmarshal(event, &request); err == nil {
+		if len(request.Path) > 0 {
+			result, err := ServerlessAPIHandler(ctx, *request)
+			jsonResult, _ := json.Marshal(result)
+			return jsonResult, err
+		}
+	}
+
+	stateEvent := &ECSInstanceStateEvent{}
+	if err := json.Unmarshal(event, &stateEvent); err == nil {
+		if len(stateEvent.Detail.DesiredStatus) > 0 {
+			result, err := ServerlessECSHandler(ctx, *stateEvent)
+			jsonResult, _ := json.Marshal(result)
+			return jsonResult, err
+		}
+	}
+
+	err := errors.New("could not find proper event handler")
+	return []byte(err.Error()), err
+}
---
+++ aws/serverless_ecs_handler.go
@@ -0,0 +1,59 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+// ECSInstanceStateEvent holds top level event details
+// full event: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs_cwe_events.html
+type ECSInstanceStateEvent struct {
+	ID     string                 `json:"id"`
+	Detail ECSInstanceStateDetail `json:"detail"`
+}
+
+// ECSInstanceStateDetail holds detail level info
+type ECSInstanceStateDetail struct {
+	Status        string `json:"status"`
+	DesiredStatus string `json:"desiredStatus"`
+	TaskArn       string `json:"taskArn"`
+}
+
+// ServerlessECSHandler runs when a game server task changes state
+func ServerlessECSHandler(ctx context.Context, event ECSInstanceStateEvent) (interface{}, error) {
+
+	if event.Detail.DesiredStatus == "STOPPED" {
+
+		// Check if item exists
+		result, err := dynamodbClient.GetItem(&dynamodb.GetItemInput{
+			TableName: &config.dynamoTable,
+			Key: map[string]*dynamodb.AttributeValue{
+				"task": {
+					S: aws.String(event.Detail.TaskArn),
+				},
+			},
+		})
+		if err != nil {
+			return err.Error(), err
+		}
+
+		// Delete item if found
+		if result.Item != nil {
+			_, err := dynamodbClient.DeleteItem(&dynamodb.DeleteItemInput{
+				TableName: &config.dynamoTable,
+				Key: map[string]*dynamodb.AttributeValue{
+					"task": {
+						S: aws.String(event.Detail.TaskArn),
+					},
+				},
+			})
+			return fmt.Sprintf("called delete on %v", event.Detail.TaskArn), err
+		}
+		return fmt.Sprintf("%v does not exist, nothing to do", event.Detail.TaskArn), nil
+	}
+
+	return fmt.Sprintf("desired state for %v is %v, ignoring", event.Detail.TaskArn, event.Detail.Status), nil
+}
